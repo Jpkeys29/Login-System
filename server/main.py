426f2ddec1180b62eb6a2bf0d1b576287d5fa9a3
Jp Keys <jpmusik29@gmail.com>
2024-07-10 08:37:03 -0400

    User class created

--- server/main.py
+++ server/main.py
@@ -3,13 +3,23 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import OperationalError
 from sqlalchemy import create_engine  # Import create_engine
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
+bcrypt = Bcrypt(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://@localhost/user_login'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:Dor!ta0822@localhost/user_login'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
+class User(db.Model):
+   useID = db.Column(db.Integer, primary_key = True)
+   first_name = db.Column(db.String(45), nullable = False)
+   last_name = db.Column(db.String(45), nullable = False)
+   email = db.Column(db.String(45), nullable = False, unique= True)
+   password = db.Column(db.String(150), nullable=False)  
 
 try:
   # Update connection URI for PostgreSQL
@@ -17,10 +27,6 @@ try:
   print("Connection to PostgreSQL database successful!")
 except OperationalError as err:
   print("Error connecting to PostgreSQL database:", err)
-
-
-# class User(db.Model):
-
 
 @app.route('/', methods=['POST'])
 def home():
@@ -32,7 +38,19 @@ def home():
     email = data.get('email')
     password = data.get('password')
 
-    return (data)
+    # Hash the password using bcrypt before storing it
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    # Create a new user and add to the database
+    new_user = User(first_name=first_name, last_name=last_name, email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully', 'user': {
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email
+    }}), 201
 
 @app.route('/dashboard')
 def dashboard():
