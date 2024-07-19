50b4e402300843a43f84bf3d601ec46db42aa8e2
Jp Keys <jpmusik29@gmail.com>
2024-07-19 07:04:35 -0400

    Hashing password and adding user to db

--- server/main.py
+++ server/main.py
@@ -6,6 +6,7 @@ import pymysql
 
 app= Flask(__name__)  #Initializes the Flask application
 cors = CORS(app)
+bcrypt = Bcrypt(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:Dor!ta0822@localhost/user_login" #Specifying the location of the local sql database
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -23,7 +24,23 @@ class User(db.Model):
 def register():
     user_data = request.get_json()
     print(user_data)
-    return user_data
+
+    first_name = user_data.get('first_name')
+    last_name = user_data.get('last_name')
+    email = user_data.get('email')
+    password = user_data['password']
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8') #Hashing password before storing it
+
+    new_user = User(first_name=first_name, last_name=last_name, email=email, password=hashed_password) #Adding new user to db
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({ 'message' : 'User created successfully', 'user': {
+        'first_name': first_name,
+        'last_name' : last_name,
+        'email' : email
+    }}), 201
 
 if __name__ == "__main__": 
     with app.app_context():
