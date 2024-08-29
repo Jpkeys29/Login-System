57bca4ffbccb50a93860a1ec741472f5d7874c54
Jp Keys <jpmusik29@gmail.com>
2024-08-29 11:16:49 -0400

    ToDoList Component

---
+++ frontend/src/components/ToDoList.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+
+const ToDoList = () => {
+    const [task, setTask] = useState('')
+    const [newTasks, setNewTasks] = useState([]);
+    const idCounter = newTasks.length === 0;
+
+    function handleAddTask() {
+        if (task !== '') {
+            setNewTasks(prevTask => [...prevTask, { id: idCounter + 1, task }])
+        }
+        setTask('');  //Clear input after adding task
+    }
+
+    const handleChange = (e) => {
+        setTask(e.target.value)
+    }
+
+    return (
+        <div>
+            <textarea value={task} onChange={handleChange}/>
+            <br/>
+            <button onClick={handleAddTask}>Add Task</button>
+            <ul>
+                {newTasks.map(task => (
+                    <li key={task.id}>{task.task}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ToDoList;
